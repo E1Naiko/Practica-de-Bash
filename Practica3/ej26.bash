45020d37f939ab02abd8c2f171c687640463895f
Nicolás Peñalba <nicolaspenalba07@gmail.com>
2026-06-02 01:08:22 -0300

    Create script for basic array operations
    Implement basic array operations using functions.

---
+++ Practica3/ej26.bash
@@ -0,0 +1,19 @@
+# Realice un script que implemente a través de la utilización de funciones las
+# operaciones básicas sobre arreglos:
+# 
+# ➢ inicializar: Crea un arreglo llamado array vacío
+# 
+# ➢ agregar_elem <parametro1>: Agrega al final del arreglo el parámetro
+# recibido
+# 
+# ➢ eliminar_elem <parametro1>: Elimina del arreglo el elemento que se
+# encuentra en la posición recibida como parámetro. Debe validar que
+# se reciba una posición válida
+# 
+# ➢ longitud: Imprime la longitud del arreglo en pantalla
+# 
+# ➢ imprimir: Imprime todos los elementos del arreglo en pantalla
+# 
+# ➢ inicializar_Con_Valores <parametro1><parametro2>: Crea un arreglo
+# con longitud <parametro1>y en todas las posiciones asigna el valor
+# <parametro2>
