1ac4066fbd5e5cbc6e781d3779b5fc558e4652e2
E1Naiko <nicolaspenalba07@gmail.com>
2026-06-02 01:42:58 -0300

    ej26 planilla hecha, despues no avance un pingo

--- Practica3/ej26.bash
+++ Practica3/ej26.bash
@@ -1,19 +1,49 @@
 # Realice un script que implemente a través de la utilización de funciones las
 # operaciones básicas sobre arreglos:
-# 
-# ➢ inicializar: Crea un arreglo llamado array vacío
-# 
-# ➢ agregar_elem <parametro1>: Agrega al final del arreglo el parámetro
-# recibido
-# 
-# ➢ eliminar_elem <parametro1>: Elimina del arreglo el elemento que se
-# encuentra en la posición recibida como parámetro. Debe validar que
-# se reciba una posición válida
-# 
-# ➢ longitud: Imprime la longitud del arreglo en pantalla
-# 
-# ➢ imprimir: Imprime todos los elementos del arreglo en pantalla
-# 
-# ➢ inicializar_Con_Valores <parametro1><parametro2>: Crea un arreglo
-# con longitud <parametro1>y en todas las posiciones asigna el valor
-# <parametro2>
+
+inicializar () {
+	# ➢ inicializar: Crea un arreglo llamado array vacío
+	
+	return
+}
+
+agregar_elem () {
+	# ➢ agregar_elem <parametro1>: Agrega al final del arreglo el parámetro
+	# recibido
+	if [ $# -eq 0 ]; then
+		echo "papito te faltan parametros"
+		return 1
+	fi
+	if [ $# -gt 1 ]; then
+		echo "EEEE para se te fue la mano"
+		return 1
+	fi
+	return
+}
+
+eliminar_elem () {
+	# ➢ eliminar_elem <parametro1>: Elimina del arreglo el elemento que se
+	# encuentra en la posición recibida como parámetro. Debe validar que
+	# se reciba una posición válida
+	if [ $# -eq 0 ]; then
+		echo "papito te faltan parametros"
+		return 1
+	fi
+	if [ $# -gt 1 ]; then
+		echo "EEEE para se te fue la mano"
+		return 1
+	fi
+	return
+}
+
+longitud () {
+	# ➢ longitud: Imprime la longitud del arreglo en pantalla
+	return
+}
+
+imprimir () {
+	# ➢ imprimir: Imprime todos los elementos del arreglo en pantalla
+	return
+}
+
+
