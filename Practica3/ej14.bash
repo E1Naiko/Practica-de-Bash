127cf89d8b4916b057ef90ba7d57421eb72fecbf
Nicolás Peñalba <nicolaspenalba07@gmail.com>
2026-06-02 00:15:07 -0300

    realmente finiquitado

--- Practica3/ej14.bash
+++ Practica3/ej14.bash
@@ -60,24 +60,28 @@ else
 	echo "Averga"
 	if [ $# -lt 3 ]; then
 		echo "Papito te quedaste corto, dale media pila"
-		exit
+		exit 1
 	fi
 	if [ $# -gt 3 ]; then
                 echo "Che pusiste mas cosas, pero me las pase por los huevos, yo te dije estoy re loco"
-		exit
+		exit 1
 	fi
 	echo " - Directorio: $1"
 	echo " - Modo: $2"
 	echo " - Cadena: $3"
 
+	if [[ ! -d "$1" ]]; then
+	    echo "$1 no es un directorio válido"
+    	exit 1
+	fi
 	case $2 in
 	"-a")
 		echo "Concateno $3 al FINAL"
-		final $1 $2 $3
+		final "$1" "$2" "$3"
 		;;
 	"-b")
 		echo "Concateno $3 al COMIENZO"
-		comienzo $1 $2 $3
+		comienzo "$1" "$2" "$3"
 		;;
 	*)
 		echo "No se que pingo es $1, papito vos estas muy equivocado"
